2e50bd45391eba701f2f5677b05070aa2c0f9cd3
alumno <alumno@LAB00PC00>
2019-10-17 21:59:06 -0300

    ...

--- ParcialLab1/src/harcodeos.c
+++ ParcialLab1/src/harcodeos.c
@@ -21,12 +21,34 @@ int addClientHardcode(Client clientE[], int length)
 	strcpy(clientE[2].clientDir,"Corrientes 2547");
 	clientE[2].isEmpty = 0;
 	clientE[2].clientID= 2;
+
 	strcpy(clientE[3].clientName,"NESTLE");
 	clientE[3].clientCuit= 308899559;
 	strcpy(clientE[3].clientLoc,"LANUS");
 	strcpy(clientE[3].clientDir,"cucha cucha 555");
 	clientE[3].isEmpty = 0;
 	clientE[3].clientID= 3;
+
+	strcpy(clientE[4].clientName,"TERRABUSI");
+	clientE[4].clientCuit= 30678142;
+	strcpy(clientE[4].clientLoc,"QUILMES");
+	strcpy(clientE[4].clientDir,"rocha 784");
+	clientE[4].isEmpty = 0;
+	clientE[4].clientID= 4;
+
+	strcpy(clientE[5].clientName,"DIA");
+	clientE[5].clientCuit= 314581253 ;
+	strcpy(clientE[5].clientLoc,"AVELLANEDA");
+	strcpy(clientE[5].clientDir,"Mitre 750");
+	clientE[5].isEmpty = 0;
+	clientE[5].clientID= 5;
+
+	strcpy(clientE[6].clientName,"QUILMES");
+	clientE[6].clientCuit= 301485756;
+	strcpy(clientE[6].clientLoc,"QUILMES");
+	strcpy(clientE[6].clientDir,"rocha 741");
+	clientE[6].isEmpty = 0;
+	clientE[6].clientID= 6;
 	return 0;
 }
 
